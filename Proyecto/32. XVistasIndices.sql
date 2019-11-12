5d2a11b7ecdaeed87cb98e8a44baa03bdd515db3
Nicolas Aguilera <nicolasaguileracontreras@gmail.com>
2019-11-12 15:01:26 -0500

    Modelo Fisico. Datos. Indices y Vistas

--- Proyecto/32. XVistasIndices.sql
+++ Proyecto/32. XVistasIndices.sql
@@ -4,6 +4,10 @@ drop index index_fecha_envio;
 drop index index_fecha_pagada; 
 drop index index_nombre_clientes; 
 
+
+--Xvistas 
+
+
 drop view Info_Productos;
 drop view Productos_vendidos;
 drop view Info_Bodegas;
